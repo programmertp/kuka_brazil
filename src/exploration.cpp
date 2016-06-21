91fd86f4f033194be3da8a581df912074bf4e0fb
Aram Azbekyan <nindz9@gmail.com>
2016-06-22 00:38:32 +0300

    Made a few changes in the node file and added dependency in CMakeLists.txt

--- src/exploration.cpp
+++ src/exploration.cpp
@@ -1,9 +1,15 @@
 #include "ros/ros.h"
 #include "kuka_brazil_msgs/GetGoal.h"
-
+)
 // Here will remain service callback function
+bool get_goal(kuka_brazil_msgs::GetGoal::Request &req,
+              kuka_brazil_msgs::GetGoal::Response &res)
+{
+    // count distance between map origin and robot pose in "/map" frame
+    double x_dist = req.robot_pose.x 
+}
 
-int main()
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "get_goal_at_map_server");
     ros::NodeHandle n;
