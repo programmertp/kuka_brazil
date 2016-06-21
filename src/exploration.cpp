832e73f4b45fd6d9f0badd06c61b1f5e9014a687
Aram Azbekyan <nindz9@gmail.com>
2016-06-21 21:55:39 +0300

    wrote service skeleton

--- src/exploration.cpp
+++ src/exploration.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+#include "ros/ros.h"
+#include "kuka_brazil_msgs/GetGoal.h"
+
+// Here will remain service callback function
 
 int main()
 {
+    ros::init(argc, argv, "get_goal_at_map_server");
+    ros::NodeHandle n;
+
+    ros::ServiceServer service = n.advertiseService("get_goal_at_map", explorer);
+    ros::spin();
+
     return 0;
 }
+
