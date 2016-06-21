a91a44eefcf4e944572dfbac5792873c17b3c6f2
Evgeniy Antonov <antonovspostbox@gmail.com>
2016-06-21 20:40:01 +0300

    Complete remake automapping node
    All changes:
    
    - globally change file "automapping.cpp";
    
    - add automapping node's params to automapping.launch.

--- src/automapping.cpp
+++ src/automapping.cpp
@@ -1,137 +1,167 @@
-#include <ros/ros.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Twist.h>
-#include <nav_msgs/GetMap.h>
 #include <RobotClass.hpp>
-#include <tf/transform_listener.h>
+#include <nav_msgs/GetPlan.h>
+#include <kuka_brazil_msgs/GetGoal.h>
 #include <cstdlib>
+#include <vector>
+#include <fstream>
 
-
-#define LOGNAME "automapping_node"
-#define NEEDED_DIST 0.5
-#define ANGLE 120.0*M_PI/180.0
-
-#define WAITING_GOAL 0
-#define ROBOT_MOVING 1
-#define WAITING_FOR_ROBOT_MOVING 2
-
-
-void locate_target(nav_msgs::OccupancyGrid &grid, geometry_msgs::Pose &robot_pose, move_base_msgs::MoveBaseGoal &a);
+#define EQUAL(A, B) A.x == B.x && A.y == B.y
 
 class AutomappingNode{
 
-	ros::NodeHandle nh;
-	RobotClass robot;	
-	ros::ServiceClient map_client;
-	move_base_msgs::MoveBaseGoal curr_goal;
-	tf::TransformListener listener_curr_pose;
-	int status;
+    ros::NodeHandle nh, nh_for_params;
+    RobotClass robot;
+    ros::ServiceClient goal_requester, pathfinder;
 
-	void mapHandler(const nav_msgs::GetMap::Response &map);
+    double safe_distance, tolerance;
+    std::string map_name, pose_params_file_name;
+
+    geometry_msgs::PoseStamped safeGoalInPlan(const nav_msgs::Path &plan);
+    double distanceBetweenPoses(const geometry_msgs::PoseStamped &pose1, const geometry_msgs::PoseStamped &pose2);
 
 public:
-	
-	AutomappingNode();
-	~AutomappingNode();
-	void doYourWork();
-	
+
+    AutomappingNode();
+    ~AutomappingNode();
+    void doYourWork();
 };
 
 
 
-AutomappingNode::AutomappingNode(){
-	
-	map_client = nh.serviceClient<nav_msgs::GetMap>("dynamic_map");
-	std::cout << LOGNAME <<  ": Initialization: success." << std::endl;	
+AutomappingNode::AutomappingNode() : nh_for_params("~"){
+    ROS_INFO_STREAM("Initialization...");
+    nh_for_params.param<double>("safe_distance", safe_distance, 0.5);
+    nh_for_params.param<double>("tolerance", tolerance, 0.1);
+    nh_for_params.param<std::string>("map_name", map_name, "~/map");
+    nh_for_params.param<std::string>("pose_params_file_name", pose_params_file_name, "~/init_pose.yaml");
+
+    goal_requester = nh.serviceClient<kuka_brazil_msgs::GetGoal>("get_goal_at_map");
+    pathfinder = nh.serviceClient<nav_msgs::GetPlan>("move_base_node/make_plan");
+    ROS_INFO_STREAM("Initialization done");
 }
+
 
 
 AutomappingNode::~AutomappingNode(){
-	map_client.shutdown();
+    goal_requester.shutdown();
+    pathfinder.shutdown();
 }
 
 
-void AutomappingNode::mapHandler(const nav_msgs::GetMap::Response &map){	
 
-	std::cout << LOGNAME << ": Start processing of the map." << std::endl; 
-
-	geometry_msgs::PoseStamped currRobotPose_in_base_link, currRobotPose;
-	currRobotPose_in_base_link.header.frame_id = "/base_link";
-	currRobotPose_in_base_link.header.stamp = ros::Time::now();
-	currRobotPose_in_base_link.pose.orientation = tf::createQuaternionMsgFromYaw(0);
-
-	bool flag = true;
-	while(flag){
-		try{	
-			flag = false;
-			listener_curr_pose.transformPose("/map", currRobotPose_in_base_link, currRobotPose);
-		}
-		catch (tf::TransformException ex){
-			flag = true;
-			ROS_ERROR("%s: %s",LOGNAME, ex.what());
-			ros::Duration(1.0).sleep();
-		}
-	}
-
-	nav_msgs::OccupancyGrid map_help = map.map;
-
-	locate_target(map_help, currRobotPose.pose, curr_goal);
-	std::cout << LOGNAME << ": locate_target() returned: x = " << curr_goal.target_pose.pose.position.x << ", y = "
-			<< curr_goal.target_pose.pose.position.y << std::endl; 
-	
-	if(curr_goal.target_pose.pose.position.x == currRobotPose.pose.position.x){
-		std::cout << LOGNAME << ": Finish work..." << std::endl;
-		std::system("rosrun map_server map_saver -f auto_map");
-		
-		std::cout << LOGNAME << ": Map was saved." << std::endl;		
-
-		ros::NodeHandle nh_for_param("~");
-		nh_for_param.setParam("red_init_x", currRobotPose.pose.position.x);
-		nh_for_param.setParam("red_init_y", currRobotPose.pose.position.y);
-		nh_for_param.setParam("red_init_angle", tf::getYaw(currRobotPose.pose.orientation));				
-		std::system("rosparam dump ~/for_ros/workspace/kuka_brazil/map/init_params.yaml /automapping_node");
-
-		std::cout << LOGNAME << ": Pose's params were saved." << std::endl;			
-
-		ros::shutdown();
-	}
-
+double AutomappingNode::distanceBetweenPoses(const geometry_msgs::PoseStamped &pose1, const geometry_msgs::PoseStamped &pose2){
+    double distance = std::pow(pose1.pose.position.x - pose2.pose.position.x, 2) +
+                      std::pow(pose1.pose.position.y - pose2.pose.position.y, 2);
+    return std::sqrt(distance);
 }
+
+
+
+geometry_msgs::PoseStamped AutomappingNode::safeGoalInPlan(const nav_msgs::Path &plan){
+    int last = plan.poses.size() - 1;
+    for(int i = last-1; i > 0; i--)
+        if(distanceBetweenPoses(plan.poses[i], plan.poses[last]) >= safe_distance)
+            return plan.poses[i];
+    return plan.poses[0];
+}
+
 
 
 void AutomappingNode::doYourWork(){
-	
-	std::cout << LOGNAME << ": Start initial rotations." << std::endl;
-	robot.rotateInPlace(ANGLE);
-	robot.rotateInPlace(ANGLE);
-	robot.rotateInPlace(ANGLE);
-	robot.rotateInPlace(-ANGLE);
-	robot.rotateInPlace(-ANGLE);
-	robot.rotateInPlace(-ANGLE);
-	std::cout << LOGNAME << ": Initial rotations have been done." << std::endl;	
 
-	nav_msgs::GetMap map_srv;
-	status = WAITING_GOAL;
-	ros::Rate sleeper(50);
-	
-	
-	while(ros::ok()){
-		if (map_client.call(map_srv)){
-			mapHandler(map_srv.response);
-			robot.moveTo(curr_goal.target_pose);
-		} 
-		else{
-			ROS_ERROR_STREAM(LOGNAME << ": Failed to call service dynamic_map!!!");
-		}
-	}
+    ROS_INFO_STREAM("Run initial rotations...");
+    for(int i = 0; i < 3; i++)
+        robot.rotateInPlace( 2*M_PI/3);
+    for(int i = 0; i < 3; i++)
+        robot.rotateInPlace(-2*M_PI/3);
+    ROS_INFO_STREAM("Initial rotations done");
+
+
+    std::string status;
+    kuka_brazil_msgs::GetGoal get_goal_srv;
+    geometry_msgs::PoseStamped curr_robot_pose;
+    nav_msgs::GetPlan get_plan_srv;
+    get_plan_srv.request.goal.header.frame_id = "map";
+    get_plan_srv.request.tolerance = tolerance;
+
+    while(ros::ok()){
+
+        ROS_INFO_STREAM("Find robot's pose...");
+        try{
+            curr_robot_pose = robot.currentPose("map");
+        }
+        catch (tf::TransformException ex){
+            ROS_ERROR("Unable to find robot's pose! Error is %s", ex.what());
+            continue;
+        }
+        ROS_INFO_STREAM("Robot's pose found");
+
+
+        get_goal_srv.request.robot_pose = curr_robot_pose.pose.position;
+        ROS_INFO_STREAM("Call " << goal_requester.getService() << " service for getting new goal...");
+        if( !goal_requester.call(get_goal_srv) ){
+            ROS_ERROR_STREAM("Unable to call " << goal_requester.getService() << " service!");
+            ros::shutdown();
+        }
+        if(EQUAL(get_goal_srv.response.goal, curr_robot_pose.pose.position)){
+            ROS_INFO_STREAM("No new goal received. Map of the environment done");
+            break;
+        }
+        ROS_INFO_STREAM("Goal from " << goal_requester.getService() << " service obtained");
+
+
+        get_plan_srv.request.start = curr_robot_pose;
+        get_plan_srv.request.goal.pose.position = get_goal_srv.response.goal;
+        ROS_INFO_STREAM("Call " << pathfinder.getService() << " service for path...");
+        if( !pathfinder.call(get_plan_srv) ){
+            ROS_ERROR_STREAM("Unable to call " << pathfinder.getService() << " service!");
+            ros::shutdown();
+        }
+        if(get_plan_srv.response.plan.poses.size() == 0){
+            ROS_INFO_STREAM("No path received");
+            get_goal_srv.request.excluded_goals.push_back(get_goal_srv.response.goal);
+            ROS_INFO_STREAM("Current goal marked as unachievable");
+            continue;
+        }
+        ROS_INFO_STREAM("Path from " << pathfinder.getService() << " service obtained");
+
+
+        ROS_INFO_STREAM("Find safe goal in path and give it to move_base");
+        status = robot.moveTo(safeGoalInPlan(get_plan_srv.response.plan));
+        ROS_INFO_STREAM("Move_base end his work with status " << status);
+        get_goal_srv.request.excluded_goals.clear();
+        if(status == "ABORTED"){
+            get_goal_srv.request.excluded_goals.push_back(get_goal_srv.response.goal);
+            ROS_INFO_STREAM("Current goal marked as unachievable");
+        }
+    }
+
+    ROS_INFO_STREAM("Saving map...");
+    map_name = "rosrun map_server map_saver -f " + map_name;
+    std::system(map_name.c_str());
+    ROS_INFO_STREAM("Map was saved");
+
+
+    ROS_INFO_STREAM("Saving information about robot's pose...");
+    std::ofstream fout(pose_params_file_name.c_str());
+    if(fout.is_open()){
+        fout << "initial_pose_x:" << curr_robot_pose.pose.position.x << std::endl;
+        fout << "initial_pose_y:" << curr_robot_pose.pose.position.y << std::endl;
+        fout << "initial_pose_a:" << tf::getYaw(curr_robot_pose.pose.orientation);
+        ROS_INFO_STREAM("Information about robot's pose was saved");
+    }
+    else
+        ROS_ERROR_STREAM("Unable to open/create \"" << pose_params_file_name << "\" file!");
+    fout.close();
+
+    ROS_INFO_STREAM("Shutdowning...");
 }
 
 
 
 int main (int argc, char **argv){
-	ros::init(argc, argv, LOGNAME);
-	std::cout << LOGNAME << ": Initialization..." << std::endl;
-	AutomappingNode node;
-	node.doYourWork();
-	return 0;
+    ros::init(argc, argv, "automapping_node");
+    AutomappingNode node;
+    node.doYourWork();
+    return 0;
 }
